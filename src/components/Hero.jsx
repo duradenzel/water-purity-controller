141f874a8b494ff44269cb8a3aa16ce51e73ad52
Denzel Huijbers <huijbers15@outlook.com>
2022-11-18 10:49:07 +0100

    removed exclamation mark

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
       </h1>
       <p className="mb-8 leading-relaxed">We find it important that everyone has access to clean drinking water. Klarity is made to check water clarity, making sure that it is safe to drink. Bodies of water can contain lots of different particles that are not detectable with the human eye. We make use of next generation technology to detect water clarity. Our device “Klearly” has highly capable TDS-sensors able to detect all kinds of invisible particles.</p>
       <div className="flex justify-center">
-        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Learn more!</button>
+        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Learn more</button>
        
       </div>
     </div>
