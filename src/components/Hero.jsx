6ce11a7f1cd6cc1fc179cdaa28f5bef0aa3ac229
Denzel Huijbers <huijbers15@outlook.com>
2022-11-18 10:37:14 +0100

    added hero picture, please work

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
 <section className="text-gray-600 body-font">
   <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-      <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"></img>
+      <img className="object-cover object-center rounded" alt="hero" src="/public/assets/picWebsite.png"></img>
     </div>
     <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
       <h1 className="title-font sm:text-4xl md:text-4xl lg:text-5xl mb-4 font-medium text-gray-900">Drink with confidence
